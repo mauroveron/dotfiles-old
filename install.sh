cab51cf2a2464ad9a6aae575f9018c77487752e7
Mauro Verón <mauroveron@gmail.com>
2012-09-02 11:18:37 -0300

    Remove echo from install script.

--- install.sh
+++ install.sh
@@ -10,12 +10,12 @@ fi
 
 echo "Installing dotfiles from '$DOTFILES'"
 
-echo ln -s $DOTFILES/zshrc $HOME/.zshrc
-echo ln -s $DOTFILES/oh-my-zsh $HOME/.oh-my-zsh
-echo ln -s $DOTFILES/vim $HOME/.vim
-echo ln -s $DOTFILES/vimrc $HOME/.vimrc
-echo ln -s $DOTFILES/my.cnf $HOME/.my.cnf
-echo ln -s $DOTFILES/gitconfig $HOME/.gitconfig
-echo ln -s $DOTFILES/screnrc $HOME/.screenrc
+ln -s $DOTFILES/zshrc $HOME/.zshrc
+ln -s $DOTFILES/oh-my-zsh $HOME/.oh-my-zsh
+ln -s $DOTFILES/vim $HOME/.vim
+ln -s $DOTFILES/vimrc $HOME/.vimrc
+ln -s $DOTFILES/my.cnf $HOME/.my.cnf
+ln -s $DOTFILES/gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/screnrc $HOME/.screenrc
 
 echo Done.
